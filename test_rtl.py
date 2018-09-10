fa6e53a32a48654855017419fbf84901df0be282
777arc <marcll@vt.edu>
2018-09-10 17:38:34 -0400

    added rtl-waterfall and test-rtl

--- test_rtl.py
+++ test_rtl.py
@@ -5,8 +5,8 @@
 #
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Fm Radio
-# Generated: Mon Sep 10 17:30:25 2018
+# Title: Test Rtl
+# Generated: Mon Sep 10 17:30:06 2018
 # GNU Radio version: 3.7.12.0
 ##################################################
 
@@ -21,10 +21,7 @@ if __name__ == '__main__':
             print "Warning: failed to XInitThreads()"
 
 from PyQt4 import Qt
-from gnuradio import analog
-from gnuradio import audio
 from gnuradio import eng_notation
-from gnuradio import filter
 from gnuradio import gr
 from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
@@ -37,12 +34,12 @@ import time
 from gnuradio import qtgui
 
 
-class fm_radio(gr.top_block, Qt.QWidget):
+class test_rtl(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Fm Radio")
+        gr.top_block.__init__(self, "Test Rtl")
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Fm Radio")
+        self.setWindowTitle("Test Rtl")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -60,22 +57,21 @@ class fm_radio(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "fm_radio")
+        self.settings = Qt.QSettings("GNU Radio", "test_rtl")
         self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
 
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 250e3
-        self.freq = freq = 101.1e6
+        self.samp_rate = samp_rate = 1e6
 
         ##################################################
         # Blocks
         ##################################################
         self.rtlsdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + '' )
         self.rtlsdr_source_0.set_sample_rate(samp_rate)
-        self.rtlsdr_source_0.set_center_freq(freq, 0)
+        self.rtlsdr_source_0.set_center_freq(100e6, 0)
         self.rtlsdr_source_0.set_freq_corr(0, 0)
         self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
         self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
@@ -86,87 +82,9 @@ class fm_radio(gr.top_block, Qt.QWidget):
         self.rtlsdr_source_0.set_antenna('', 0)
         self.rtlsdr_source_0.set_bandwidth(0, 0)
 
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=48000*4,
-                decimation=250000,
-                taps=None,
-                fractional_bw=None,
-        )
-        self.qtgui_waterfall_sink_x_0_0_1 = qtgui.waterfall_sink_f(
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
         	1024, #size
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	0, #fc
-        	48e3, #bw
-        	"", #name
-                1 #number of inputs
-        )
-        self.qtgui_waterfall_sink_x_0_0_1.set_update_time(0.10)
-        self.qtgui_waterfall_sink_x_0_0_1.enable_grid(False)
-        self.qtgui_waterfall_sink_x_0_0_1.enable_axis_labels(True)
-
-        if not True:
-          self.qtgui_waterfall_sink_x_0_0_1.disable_legend()
-
-        if "float" == "float" or "float" == "msg_float":
-          self.qtgui_waterfall_sink_x_0_0_1.set_plot_pos_half(not False)
-
-        labels = ['', '', '', '', '',
-                  '', '', '', '', '']
-        colors = [0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        for i in xrange(1):
-            if len(labels[i]) == 0:
-                self.qtgui_waterfall_sink_x_0_0_1.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_waterfall_sink_x_0_0_1.set_line_label(i, labels[i])
-            self.qtgui_waterfall_sink_x_0_0_1.set_color_map(i, colors[i])
-            self.qtgui_waterfall_sink_x_0_0_1.set_line_alpha(i, alphas[i])
-
-        self.qtgui_waterfall_sink_x_0_0_1.set_intensity_range(-140, 10)
-
-        self._qtgui_waterfall_sink_x_0_0_1_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0_0_1.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_0_0_1_win)
-        self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
-        	1024, #size
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	freq, #fc
-        	samp_rate, #bw
-        	"", #name
-                1 #number of inputs
-        )
-        self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
-        self.qtgui_waterfall_sink_x_0.enable_grid(False)
-        self.qtgui_waterfall_sink_x_0.enable_axis_labels(True)
-
-        if not True:
-          self.qtgui_waterfall_sink_x_0.disable_legend()
-
-        if "complex" == "float" or "complex" == "msg_float":
-          self.qtgui_waterfall_sink_x_0.set_plot_pos_half(not True)
-
-        labels = ['', '', '', '', '',
-                  '', '', '', '', '']
-        colors = [0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        for i in xrange(1):
-            if len(labels[i]) == 0:
-                self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_waterfall_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_waterfall_sink_x_0.set_color_map(i, colors[i])
-            self.qtgui_waterfall_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
-
-        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-        	5000, #size
-        	48e3, #samp_rate
+        	samp_rate, #samp_rate
         	"", #name
         	1 #number of inputs
         )
@@ -199,9 +117,12 @@ class fm_radio(gr.top_block, Qt.QWidget):
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
                   1.0, 1.0, 1.0, 1.0, 1.0]
 
-        for i in xrange(1):
+        for i in xrange(2):
             if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+                if(i % 2 == 0):
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                else:
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
             else:
                 self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
             self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
@@ -212,26 +133,16 @@ class fm_radio(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.audio_sink_0 = audio.sink(48000, '', True)
-        self.analog_wfm_rcv_0 = analog.wfm_rcv(
-        	quad_rate=192000,
-        	audio_decimation=4,
-        )
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_wfm_rcv_0, 0), (self.audio_sink_0, 0))
-        self.connect((self.analog_wfm_rcv_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.analog_wfm_rcv_0, 0), (self.qtgui_waterfall_sink_x_0_0_1, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_wfm_rcv_0, 0))
-        self.connect((self.rtlsdr_source_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
-        self.connect((self.rtlsdr_source_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.rtlsdr_source_0, 0), (self.qtgui_time_sink_x_0, 0))
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "fm_radio")
+        self.settings = Qt.QSettings("GNU Radio", "test_rtl")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
@@ -241,18 +152,10 @@ class fm_radio(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
-        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
-
-    def get_freq(self):
-        return self.freq
-
-    def set_freq(self, freq):
-        self.freq = freq
-        self.rtlsdr_source_0.set_center_freq(self.freq, 0)
-        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
 
-def main(top_block_cls=fm_radio, options=None):
+def main(top_block_cls=test_rtl, options=None):
 
     from distutils.version import StrictVersion
     if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
